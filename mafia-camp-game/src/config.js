921b0971f1563d5019ee318b1310a5926607b9cf
matybarton <matybarton@seznam.cz>
2026-07-17 13:20:03 +0200

    Update config.js

--- mafia-camp-game/src/config.js
+++ mafia-camp-game/src/config.js
@@ -2,5 +2,5 @@
 // Jdi na https://supabase.com -> vytvoř projekt -> Settings -> API
 // Zkopíruj Project URL a anon public key
 
-export const supabaseUrl = https://gaekfkxdasbkypeptevs.supabase.co;
-export const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZWtma3hkYXNia3lwZXB0ZXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzk2MjMsImV4cCI6MjA5OTg1NTYyM30.FOBwLM64DkxQoCFJjC-gh7JDU3walAHlmX33s1MmpLA;
+export const supabaseUrl = "https://gaekfkxdasbkypeptevs.supabase.co";
+export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZWtma3hkYXNia3lwZXB0ZXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzk2MjMsImV4cCI6MjA5OTg1NTYyM30.FOBwLM64DkxQoCFJjC-gh7JDU3walAHlmX33s1MmpLA";
