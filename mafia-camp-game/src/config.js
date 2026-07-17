863ba7dbad7f762eab7c3f1ebbd4be4a8f156198
matybarton <matybarton@seznam.cz>
2026-07-17 13:25:54 +0200

    Delete mafia-camp-game directory

--- mafia-camp-game/src/config.js
+++
@@ -1,6 +0,0 @@
-// ⚠️ VYPLŇ SVOJE SUPABASE ÚDAJE!
-// Jdi na https://supabase.com -> vytvoř projekt -> Settings -> API
-// Zkopíruj Project URL a anon public key
-
-export const supabaseUrl = "https://gaekfkxdasbkypeptevs.supabase.co";
-export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZWtma3hkYXNia3lwZXB0ZXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzk2MjMsImV4cCI6MjA5OTg1NTYyM30.FOBwLM64DkxQoCFJjC-gh7JDU3walAHlmX33s1MmpLA";
